d499fa2a8238c9ce03803b06e39d038f8808e4b3
lukaszkwasny <lukasz.kwasny94@gmail.com>
2020-03-23 17:38:10 +0100

    Implementing bootsrap to design Form and Show-result

--- GuestBook/main.js
+++ GuestBook/main.js
@@ -3,41 +3,51 @@ var form = document.getElementById('myForm');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     showresult();
-})
+});
 
 function showresult() {
-    var text = document.getElementById("text").value;
-    var contents = document.getElementById("contents").value;
-    var signature = document.getElementById("signature").value;
-    var gender = document.querySelector('input[name="gender"]:checked').value;
-    var email = document.getElementById("mail").value;
-    var checkBox = document.getElementById("agree");
 
-    addRow(text, signature, gender, contents, email, checkBox)
+    var
+        title = document.getElementById("title").value,
+        contents = document.getElementById("contents").value,
+        signature = document.getElementById("signature").value,
+        gender = document.querySelector('input[name="gender"]:checked').value,
+        email = document.getElementById("mail").value,
+        checkBox = document.getElementById("agree");
 
-}
+    addRow(title, signature, gender, contents, email, checkBox)
 
-function addRow(text, signature, gender, contents, email, checkBox) {
+};
+
+function addRow(title, signature, gender, contents, email, checkBox) {
+
     var div = document.createElement('div');
 
 
     div.innerHTML =
-        "<div>" +
-        "<p><strong>Signature: </strong>" + signature + "</p>" +
-        "<p><strong>E-mail: </strong>" + (checkBox.checked === true ? email : "hidden") + "</p>" +
-        "<p><strong>Contents: </strong>" + contents + "</p>" +
-        "<p><strong>Text: </strong>" + text + "</p>" +
-        "<p><strong>Gender: </strong>" + (gender === 'm' ? 'Man' : "Woman") + "</p>" +
-        "</div>";
+
+             "<div class='card mt-2'>"+
+                "<div class='card-body mt-4'>" +
+                    "<dl class='row'>"+
+                        "<dt class='col-sm-3'>Title: </dt>" +
+                        "<dd class='col-sm-9'>" + title + "</dd>" +
+                        "<dt class='col-sm-3'>Signature: </dt>" +
+                        "<dd class='col-sm-9'>" + signature + "</dd>" +
+                        "<dt class='col-sm-3'>Contents: </dt>" +
+                        "<dd class='col-sm-9'>" + contents + "</dd>" +
+                        "<dt class='col-sm-3'>Gender: </dt>" +
+                        "<dd class='col-sm-9'>" + gender + "</dd>" +
+                        "<dt class='col-sm-3'>E-mail: </dt>" +
+                        "<dd class='col-sm-9'>" + (checkBox.checked === true ? email : "hidden") + "</dd>" +
+                    "</dl>" +
+                "</div>" +
+            "</div>";
+
 
     document.getElementById('content').prepend(div);
     // wynik od dolu  parent.append(p);
     // gora parent.prepend(span);
-}
-
-
-
-
+};
 
 
 
