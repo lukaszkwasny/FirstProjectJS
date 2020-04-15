d296cd90667b74cc158ffb98bafe0852614769aa
lukaszkwasny <lukasz.kwasny94@gmail.com>
2020-04-15 17:13:01 +0200

    Add Validete.js and add to main.js validationSchemes

--- GuestBook/js/main.js
+++ GuestBook/js/main.js
@@ -1,55 +1,86 @@
-var form = document.getElementById('myForm');
+var form = document.getElementById('myForm'),
+    source = document.getElementById("tpl").innerHTML,
+    template = Handlebars.compile(source),
+    validationSchemes = {
 
-var source = document.getElementById("tpl").innerHTML,
-    template = Handlebars.compile(source);
+        title: {
+            length: {
+                minimum: 5,
+                maximum: 25
+            },
+            presence: {
+                allowEmpty: false
+            }
+        },
+        contents: {
+            length: {
+                minimum: 10
+            },
+            presence: {
+                allowEmpty: false
+            }
+        },
+        signature: {
+            length: {
+                minimum: 3,
+                maximum: 30
+            },
+            presence: {
+                allowEmpty: false
+            }
+        },
+        gender: {
+            inclusion: [
+                'woman',
+                'man'
+            ],
+            presence: {
+                allowEmpty: false
+            }
+        },
+        email: {
+            email: true,
+            presence: {
+                allowEmpty: false
+            }
+        }
 
+    };
+
+//stop page reloading
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     showresult();
 });
 
+// catch date from form
 function showresult() {
 
-    var
-        title = document.getElementById("title").value,
-        contents = document.getElementById("contents").value,
-        signature = document.getElementById("signature").value,
-        gender = document.querySelector('input[name="gender"]:checked').value,
-        email = document.getElementById("mail").value,
-        checkBox = document.getElementById("agree");
+    var data = {
+        title: document.getElementById("title").value,
+        contents: document.getElementById("contents").value,
+        signature: document.getElementById("signature").value,
+        gender: document.querySelector('input[name="gender"]:checked').value,
+        email: document.getElementById("mail").value,
+        checkBox: document.getElementById("agree")
+    };
 
-    addRow(title, signature, gender, contents, email, checkBox)
+    var validationResult = validate(data, validationSchemes);
 
+    if (!validationResult) {
+        addRow(data)
+    } else {
+        console.log(validationResult);
+    }
 };
 
-function addRow(title, signature, gender, contents, email, checkBox) {
+// add row with date from form
+function addRow(data) {
 
     var div = document.createElement('div');
 
-    var date = {
-        title: title,
-        contents: contents,
-        signature: signature,
-        gender: gender,
-        email: email,
-        checkBox: checkBox.checked
-
-    }
-
-
-    var html = template(date);
-    div.innerHTML = html;
+    div.innerHTML = template(data);
 
     document.getElementById('content').prepend(div);
 
 };
-
-
-
-
-
-
-
-
-
-
